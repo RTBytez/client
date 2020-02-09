4c28cb72e0d6d36d1704969c31801d56fbe9ad9d
CzE <cze@clashmc.co>
2020-02-08 20:44:58 -0500

    Begun work experimenting with windows. Fun

---
+++ src/main/java/com/rtbytez/client/RTBytezToolWindow.java
@@ -0,0 +1,23 @@
+package com.rtbytez.client;
+
+import com.intellij.openapi.wm.ToolWindow;
+
+import javax.swing.*;
+
+public class RTBytezToolWindow {
+
+    private JPanel rTBytezToolWindowContent;
+    private JButton button1;
+    private JScrollPane panel2;
+    private JButton button2;
+    private JButton toggle;
+
+    public RTBytezToolWindow(ToolWindow toolWindow) {
+
+    }
+
+    public JPanel getContent() {
+        return rTBytezToolWindowContent;
+    }
+
+}
