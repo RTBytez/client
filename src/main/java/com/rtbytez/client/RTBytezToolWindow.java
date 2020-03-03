941e8a6eca53f28e678340444abf553821da228e
Zackary Wake <zjw4373@rit.edu>
2020-03-03 09:16:51 -0500

    Added more buttons to the RTBytez Tool Window.

--- src/main/java/com/rtbytez/client/RTBytezToolWindow.java
+++ src/main/java/com/rtbytez/client/RTBytezToolWindow.java
@@ -3,14 +3,18 @@ package com.rtbytez.client;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.util.ui.components.BorderLayoutPanel;
+import com.rtbytez.client.dialogs.ConnectDialog;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class RTBytezToolWindow {
 
     private JPanel rTBytezToolWindowContent;
     private JPanel actionToolbarPanel;
+    private BorderLayoutPanel borderLayoutPanel;
 
     public RTBytezToolWindow(ToolWindow toolWindow) {
 
@@ -21,18 +25,76 @@ public class RTBytezToolWindow {
     }
 
     private void createUIComponents() {
-        ActionGroup group = new DefaultActionGroup(
-                new AnAction("Connect", "Connect to RTBytez Server", AllIcons.Actions.Execute) {
-                    @Override
-                    public void actionPerformed(@NotNull AnActionEvent e) {
+        Dimension size = new Dimension(198, 28);
+        AnAction connectButton = new AnAction("Connect", "Connect to RTBytez Server", AllIcons.Actions.Execute) {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e) {
+                ConnectDialog connection = new ConnectDialog();
+                connection.getUriText();
+            }
+        };
+        AnAction disconnectButton = new AnAction("Disconnect", "Disconnect from RTBytez Server", AllIcons.Actions.Exit) {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e) {
 
-                    }
-                }
+            }
+        };
+        AnAction refreshButton = new AnAction("Refresh", "Refresh", AllIcons.Actions.Refresh) {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e) {
+
+            }
+        };
+        AnAction filesButton = new AnAction("Files", "Files", AllIcons.Actions.NewFolder) {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e) {
+
+            }
+        };
+        AnAction membersButton = new AnAction("Members", "Members", AllIcons.Actions.Profile) {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e) {
+
+            }
+        };
+        AnAction conflictsButton = new AnAction("Conflicts", "Conflicts", AllIcons.Actions.Cancel) {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e) {
+
+            }
+        };
+        AnAction commitButton = new AnAction("Commit", "Commit", AllIcons.Actions.Menu_saveall) {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e) {
+
+            }
+        };
+        AnAction pushButton = new AnAction("Push", "Push", AllIcons.Actions.Upload) {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e) {
+
+            }
+        };
+        AnAction createRoomButton = new AnAction("Create Room", "Create Room", AllIcons.Actions.New) {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e) {
+
+            }
+        };
+        AnAction serverManagerButton = new AnAction("Server Manager", "Server Manager", AllIcons.Actions.Lightning) {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e) {
+
+            }
+        };
+        ActionGroup group = new DefaultActionGroup(
+                connectButton, refreshButton, createRoomButton, serverManagerButton, filesButton, membersButton,
+                conflictsButton, commitButton, pushButton
         );
+
         ActionManager actionManager = ActionManager.getInstance();
         ActionToolbar actionToolbar = actionManager.createActionToolbar(ActionPlaces.UNKNOWN, group, false);
         actionToolbarPanel = new JPanel();
-        actionToolbarPanel.add(actionToolbar.getComponent());
     }
 }
 
