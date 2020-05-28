9ff552f337dca6fa97b04289ca060cd19153f6f2
Zackary Wake <zjw4373@g.rit.edu>
2020-05-28 19:14:09 -0400

    Fully implemented real-time connection tracking for Tool Window Buttons

--- src/main/java/com/rtbytez/client/actions/URIGetter.java
+++ src/main/java/com/rtbytez/client/actions/URIGetter.java
@@ -3,39 +3,34 @@ package com.rtbytez.client.actions;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.rtbytez.client.ConnectSanitizedInput;
+import com.rtbytez.client.RTBytezClient;
+import com.rtbytez.client.ui.dialogs.ConnectDialog;
 import org.jetbrains.annotations.NotNull;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.ArrayList;
 
 public class URIGetter extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
-        //TODO: Fix
-        //System.out.println("Point A");
-        //RTBytezClient client = RTBytezClient.getInstance();
-        //CredentialsGetter credentialsGetter = new CredentialsGetter();
-        //try {
-        //    ConnectSanitizedInput input = retrieveURI();
-        //    if (input != null) {
-        //        ArrayList<String> credentials = credentialsGetter.retrieveCredentials();
-        //        input.setUsername(credentials.get(0));
-        //        input.setPassword(credentials.get(1));
-        //        client.getSocketClient().connect(input.getHost());
-        //    }
-        //} catch (URISyntaxException e) {
-        //    e.printStackTrace();
-        //}
+        System.out.println("Point A");
+        RTBytezClient client = RTBytezClient.getInstance();
+        CredentialsGetter credentialsGetter = new CredentialsGetter();
+        ConnectSanitizedInput input = retrieveURI();
+        if (input != null) {
+            ArrayList<String> credentials = credentialsGetter.retrieveCredentials();
+            input.setUsername(credentials.get(0));
+            input.setPassword(credentials.get(1));
+        }
     }
 
     public ConnectSanitizedInput retrieveURI() {
-        //TODO: Fix
-        //ConnectDialog dialog = new ConnectDialog();
-        //boolean ok = dialog.showAndGet(); // Hangs
-        //if (ok) {
-        //    return dialog.getUriText();
-        //} else {
-        //    return null;
-        //}
-        throw new NotImplementedException();
+        ConnectDialog dialog = new ConnectDialog();
+        boolean ok = dialog.showAndGet(); // Hangs
+        if (ok) {
+            return dialog.getUriText();
+        } else {
+            return null;
+        }
     }
 }
--- src/main/java/com/rtbytez/client/socket/Peer.java
+++ src/main/java/com/rtbytez/client/socket/Peer.java
@@ -54,6 +54,9 @@ public class Peer {
     }
 
     public boolean isConnected() {
+        if (status != null) {
+            System.out.println(status == SocketStatus.CONNECTED);
+        }
         return this.status == SocketStatus.CONNECTED;
     }
 
@@ -74,6 +77,7 @@ public class Peer {
     }
 
     public void setStatus(SocketStatus status) {
+        System.out.println("Set socket status to " + status.name());
         this.status = status;
     }
 
