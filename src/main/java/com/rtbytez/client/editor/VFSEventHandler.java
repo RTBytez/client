330dc3f75897d78f8659b20142e3601521525cd3
CzE <cze@clashmc.co>
2020-05-31 20:57:06 -0400

    Don't process VFS events when the client is not operational

--- src/main/java/com/rtbytez/client/editor/VFSEventHandler.java
+++ src/main/java/com/rtbytez/client/editor/VFSEventHandler.java
@@ -30,15 +30,17 @@ public class VFSEventHandler {
             public void after(@NotNull List<? extends VFileEvent> events) {
                 events.forEach(vFileEvent -> {
                     Console.log("Caught a " + vFileEvent.toString() + " event");
-                    if (vFileEvent instanceof VFileCreateEvent) {
-                        VFileCreateEvent vFileCreateEvent = (VFileCreateEvent) vFileEvent;
-                        String path = toRelPath(vFileCreateEvent.getPath());
-                        client.getPeer().emit(new RTPFileRequestCreate("file", path));
-                        client.getPeer().emit(new RTPFileRequestAddLine("file", path, 1));
-                    } else if (vFileEvent instanceof VFileDeleteEvent) {
-                        VFileDeleteEvent vFileDeleteEvent = (VFileDeleteEvent) vFileEvent;
-                        String path = toRelPath(vFileDeleteEvent.getPath());
-                        client.getPeer().emit(new RTPFileRequestDelete("file", path));
+                    if (client.isOperational()) {
+                        if (vFileEvent instanceof VFileCreateEvent) {
+                            VFileCreateEvent vFileCreateEvent = (VFileCreateEvent) vFileEvent;
+                            String path = toRelPath(vFileCreateEvent.getPath());
+                            client.getPeer().emit(new RTPFileRequestCreate("file", path));
+                            client.getPeer().emit(new RTPFileRequestAddLine("file", path, 1));
+                        } else if (vFileEvent instanceof VFileDeleteEvent) {
+                            VFileDeleteEvent vFileDeleteEvent = (VFileDeleteEvent) vFileEvent;
+                            String path = toRelPath(vFileDeleteEvent.getPath());
+                            client.getPeer().emit(new RTPFileRequestDelete("file", path));
+                        }
                     }
                     //assert vFileEvent.getFile() != null;
                     //PsiFile psiFile = PsiManager.getInstance(project).findFile(vFileEvent.getFile());
